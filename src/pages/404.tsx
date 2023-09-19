74ff70ad39cc6c7cffc24dd4d6dd45cad40d7eea
Rifat <takdircoders@gmail.com>
2023-09-19 14:15:12 +0600

    initialize

---
+++ src/pages/404.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "../styles/ErroprPages.module.css";
+import { useRouter } from "next/router";
+import { Button } from "flowbite-react";
+
+const ErrorPage = () => {
+  const router = useRouter();
+
+  // setTimeout(() => {
+  //   router.push("/");
+  // }, 5000);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div>
+        <h3 className="font-semibold text-2xl py-2 text-center ">Error</h3>
+        <h1
+          className={`font-semibold text-5xl py-2 text-center ${styles.error}`}
+        >
+          404
+        </h1>
+        <div className="font-semibold text-2xl py-2 text-center">
+          Ooops!!! The page is not found
+        </div>
+        <Link href="/" className="back-home flex justify-center">
+          <Button>Back To Home</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
